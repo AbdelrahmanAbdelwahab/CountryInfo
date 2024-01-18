2774d18352fbbfd420cab2e7643822067cf005cf
Abdelrahman Abdelwahab <abdelrahman.abdelwahab@mooney.it>
2024-01-18 09:28:16 +0100

    Delete Main.storyboard

---
+++ CountryInfo/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CountryInfo
+//
+//  Created by ABDELWAHAB on 18/01/24.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+      
+    }
+
+}
